3b31077234baecf24eb7e8ac69011297e39f92c6
Theenadayalan K <theenadayalan.k@zohocorp.com>
2019-06-11 12:18:17 +0530

    title fix

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 import Header from './../Header';
 
 const Layout = ({ children }) => {
   return (
     <div>
+      <Helmet>
+        {/* Title */}
+        <title>Theenadayalan | Front End Engineer</title>
+      </Helmet>
       <Header />
       <div className="navbar-nav">{children}</div>
     </div>
